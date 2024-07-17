1b51d12c3b0c26461e89e4fb4d77a7a2cdb0ff43
tayha1011 <160646200+KthDlsTK@users.noreply.github.com>
2024-07-17 17:57:23 +0900

    HHH

--- HandMouse/HandMouse.py
+++ HandMouse/HandMouse.py
@@ -4,15 +4,8 @@ from PIL import ImageFont, ImageDraw, Image
 import numpy as np
 import pyautogui  
 
-
 mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands.Hands(
-    static_image_mode=False,
-    max_num_hands=2,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5,
-    use_gpu=True
-)
+mp_hands = mp.solutions.hands
 mp_drawing_styles = mp.solutions.drawing_styles
 
 
--- hand_detector.py
+++ hand_detector.py
@@ -13,14 +13,14 @@ class handDetector():
         self.detectionCon = detectionCon
         self.trackCon = trackCon
  
-        self.mpHands = mp.solutions.hands.Hands(
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(
             static_image_mode=False,
             max_num_hands=2,
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5,
             use_gpu=True
         )
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
  
